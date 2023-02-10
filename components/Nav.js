bafe04e4a496b9d5178424ab84bd4a069141b018
K. Canamar | Remote Full Stack Software Engineer <99228411+kcanamar@users.noreply.github.com>
2023-02-10 15:02:15 -0800

    creates mobile responsive nav, w/ tray

--- components/Nav.js
+++ components/Nav.js
@@ -2,6 +2,8 @@ import Link from "next/link"
 import { useState } from "react"
 import { useDevContext } from "@/utils/devContext"
 import styles from "../styles/components/Nav.module.css"
+import { FiMenu } from "react-icons/fi"
+import { MdClose } from "react-icons/md"
 
 export default function Nav() {
     // desructure the value from context
@@ -9,11 +11,42 @@ export default function Nav() {
     // destructure property needed for component
     const { name } = value.devContent
 
-    const [active, setActive] = useState('#')
+    const [active, setActive] = useState({
+        visible: "",
+        show: false,
+        mobile: false,
+        star: ""
+    })
+
+    const MenuHandler = () => {
+        if (active.visible === "") {
+            setActive({
+                visible: "true",
+                show: true
+            })
+        } else {
+            setActive({
+                visible: "",
+                show: false
+            })
+        }
+
+    }
+
+    const LinkHandler = (event) => {
+        if (active.visible === "true" || active.show === true) {
+            setActive({
+                visible: "",
+                show: false
+            })
+        }
+
+    }
+
     return (
         <header className={styles.header}>
             
-            <div className={styles.left}>
+            <div className={styles.logo}>
                 <Link 
                     href="/"
                     >
@@ -21,37 +54,62 @@ export default function Nav() {
                 </Link>
             </div>
 
-            <button></button>
+            <button 
+                className={styles.navToggle}
+                data-visible={active.visible}
+                onClick={() => MenuHandler()}
+            >
+                {active.show ? <MdClose/> : <FiMenu/> }
+            </button>
 
 
-            <div className={styles.right}>
-                <Link 
-                    href="/about"
-                    >
-                        About
-                </Link>
-                <Link 
-                    href="/projects"
-                    >
-                        Projects
-                </Link>
-                <Link 
-                    href="/contact"
-                    >
-                        Contact
-                </Link>
-                <Link 
-                    href="/blog"
-                    >
-                        Blog
-                </Link>
+            <ul 
+                className={styles.primaryNav}
+                data-visible={active.visible}
+            >
+                <li >
+                    <Link
+                        href="/about"
+                        onClick={() => LinkHandler()}
+
+                        >
+                            About
+                    </Link>
+                </li>
+                <li>
+                    <Link
+                        href="/projects"
+                        onClick={() => LinkHandler()}
+                        >
+                            Projects
+                    </Link>
+                </li>
+                <li>
+                    <Link
+                        href="/contact"
+                        onClick={() => LinkHandler()}
+                        >
+                            Contact
+                    </Link>
+                </li>
+                <li>
+                    <Link
+                        href="/blog"
+                        onClick={() => LinkHandler()}
+                        >
+                            Blog
+                    </Link>
+                </li>
                 {/* Todo link to google pdf view of current resume */}
-                <Link 
-                    href=""
-                    >
-                        Resume
-                </Link>
-            </div>
+                <li>
+                    <Link
+                        href=""
+                        // todo need a handler to close menu after download
+                        >
+                            Resume
+                    </Link>
+                </li>
+            </ul>
 
         </header>
     )
