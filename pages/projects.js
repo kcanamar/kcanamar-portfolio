9e6469fe207719b911220066d08cd37e6d2fd166
K. Canamar | Remote Full Stack Software Engineer <99228411+kcanamar@users.noreply.github.com>
2023-02-14 19:00:59 -0800

    adds resume

--- pages/projects.js
+++ pages/projects.js
@@ -1,6 +1,25 @@
-
+import dbConnect from '@/utils/dbConnect'
+import AboutInfo from '@/models/about'
+import ProjectInfo from '@/models/project'
+import TalentInfo from '@/models/talent'
 
 export default function Projects() {
 
     return <h1>Projects Page</h1>
 }
+
+export async function getServerSideProps() {
+    await dbConnect()
+  
+    /* find all the data in our database */
+    const result = await AboutInfo.find({}).populate("projects")
+    const dev = JSON.parse(JSON.stringify(result[0]))
+
+    const result2 = await ProjectInfo.find({}).populate("stack")
+    const projects = JSON.parse(JSON.stringify(result2))
+
+    const result3 = await TalentInfo.find({})
+    const talents = JSON.parse(JSON.stringify(result3))
+  
+    return { props: { dev, projects, talents } }
+  }
